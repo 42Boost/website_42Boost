e409bf3343f92df8f60f528fb53721ff4858698d
MatLBS <135004599+MatLBS@users.noreply.github.com>
2025-09-29 10:30:56 +0200

    add a sitemap

--- app/routes.ts
+++ app/routes.ts
@@ -3,8 +3,8 @@ import { type RouteConfig, index, route } from "@react-router/dev/routes";
 export default [
   index("routes/home.tsx"),
   route("nos-services", "routes/nos-services.tsx"),
-  route("notre-structure", "routes/notre-structure.tsx"),
   route("notre-procede", "routes/notre-procede.tsx"),
+  route("notre-structure", "routes/notre-structure.tsx"),
   route("junior-entreprises", "routes/junior-entreprises.tsx"),
   route("contact", "routes/contact.tsx"),
   route("mentions-legales", "routes/mentions-legales.tsx"),
